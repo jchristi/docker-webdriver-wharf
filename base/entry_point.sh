32122ab670c3d0bbc74bddcc3a2d73528b98a668
Jeremy Christian <jchristi@redhat.com>
2016-04-22 01:07:44 -0400

    Updates to base selenium startup script

--- base/entry_point.sh
+++ base/entry_point.sh
@@ -1,30 +1,27 @@
 #!/bin/sh
+export PATH="/usr/bin:/opt/firefox:/opt/bin:$PATH"
 export DISPLAY=:99
-export SE_OPTS="-ensureCleanSession -trustAllSSLCertificates"
 export SCREEN_WIDTH=1280
 export SCREEN_HEIGHT=1024
 export SCREEN_DEPTH=16
 export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
-export PATH="/usr/bin:/opt/firefox:/opt/bin:$PATH"
-LOG="/var/log/selenium/xvfb.log"
+export SE_OPTS="-ensureCleanSession -trustAllSSLCertificates"
+export X_OPTS="$DISPLAY -ac -screen 0 $GEOMETRY +extension RANDR"
+export X_LOG="/var/log/selenium/xvfb.log"
 
 function shutdown {
-  kill -s SIGTERM $NODE_PID
-  wait $NODE_PID
+  kill -s SIGTERM $NODE_PID $X_PID
+  wait $NODE_PID $X_PID
 }
 
 if [ ! -z "$SE_OPTS" ]; then
   echo "appending selenium options: ${SE_OPTS}"
 fi
 
-xvfb-run \
-  -e $LOG \
-  --wait=5 \
-  --server-num=99 \
-  --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
-  java -jar /opt/selenium/selenium-server-standalone.jar \
-  ${SE_OPTS} &
+Xvfb $X_OPTS >>$X_LOG 2>&1 &
+X_PID=$!
+java -jar /opt/selenium/selenium-server-standalone.jar ${SE_OPTS} &
 NODE_PID=$!
 
 trap shutdown SIGTERM SIGINT
-wait $NODE_PID
+wait $NODE_PID $X_PID
